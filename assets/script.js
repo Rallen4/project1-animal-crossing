828a25cf7f88635261c088b40204ceac9939f7ac
kelliekumasaka <kelliek3@gmail.com>
2021-10-05 12:55:27 -0700

    sorting basics

--- assets/script.js
+++ assets/script.js
@@ -172,17 +172,20 @@ function getVillagers(){
         .then(function(response){
             return response.json();
         })
-        .then(function(data){
-            console.log(data.name['US-en']);
-            console.log(data.personality);
-            console.log(data.birthday);
-            console.log(data.hobby);
-            // villagerIcon.setAttribute("src", data.icon);
+        .then(function (data){
+            renderVillagers(data);
+        }
+        )
+    } 
+}
+
+function renderVillagers(data){
+    console.log(data)
             var newRow = villagerTable.insertRow(1);
             var colCheckbox = newRow.insertCell(0)
             // creates a checkbox in that cell
             colCheckbox.innerHTML = `<label>
-            <input id="fish" onclick="toggleCompletion()" type="checkbox" data-completed: false/>
+            <input id="villagerCheck" onclick="toggleCompletion()" type="checkbox" data-completed: false/>
             <span></span>
             </label>`
             var colImg = newRow.insertCell(1)
@@ -191,15 +194,77 @@ function getVillagers(){
             var colName = newRow.insertCell(2);
             // add name to name cell
             colName.innerText = data.name['US-en'];
-            var colLocation = newRow.insertCell(3);
+            var colPersonality = newRow.insertCell(3);
             // add personality
-            colLocation.innerText = data.personality;
-            var colLocation = newRow.insertCell(4);
+            colPersonality.innerText = data.personality;
+            var colBirthday = newRow.insertCell(4);
             // add birthday
-            colLocation.innerText = data.birthday;
-            var colLocation = newRow.insertCell(5);
+            colBirthday.innerText = data.birthday;
+            var colHobby = newRow.insertCell(5);
             // add hobby
-            colLocation.innerText = data.hobby;
-        })
-    } 
+            colHobby.innerText = data.hobby;
+}
+
+function sortTable(n) {
+
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("villagerTable");
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // Start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /* Loop through all table rows (except the
+      first, which contains table headers): */
+      for (i = 1; i < (rows.length - 1); i++) {
+        // Start by saying there should be no switching:
+        shouldSwitch = false;
+        /* Get the two elements you want to compare,
+        one from current row and one from the next: */
+        x = rows[i].getElementsByTagName("td")[n+2];
+        y = rows[i + 1].getElementsByTagName("td")[n+2];
+        /* Check if the two rows should switch place,
+        based on the direction, asc or desc: */
+        if (dir == "asc") {
+            console.log(x.innerHTML)
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark that a switch has been done: */
+        rows[i].parentNode.insertBefore(rows[i + 1],rows[i]);
+        switching = true;
+        // Each time a switch is done, increase this count by 1:
+        switchcount ++;
+      } else {
+        /* If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again. */
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }renderRows();
+}
+
+function renderRows(rows){
+    var table = document.getElementById("villagerTable");
+    for (let i = 1; i < table.children("tr").length; i++) {
+        table.children("tr")[i] = rows[i-1]
+    }
 }
