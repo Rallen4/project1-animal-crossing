fe0eeefe3db4ce7c5abc693ee06ac7762e1480c7
SpecsNStats <specsnstats@gmail.com>
2021-10-04 21:35:33 -0600

    function to delete all previous rows

--- assets/script.js
+++ assets/script.js
@@ -23,6 +23,11 @@ function toggleCompletion(type, id) {
 }
 
 function getFish() {
+    // function that deletes all the rows of the previous function
+    var rowCount = mainContent.rows.length;
+    for (var i = rowCount - 1; i > 0; i--) {
+        mainContent.deleteRow(i);
+    }
     // define URL for API
     var requestUrl = 'https://acnhapi.com/v1/fish/';
     // fetching data of API
@@ -163,13 +168,6 @@ function getVillagers() {
     }
 }
 
-// getFish();
-// getSeaCreatures();
-// getBugs();
-// getFossils();
-// getSongs();
-// getArt();
-// getVillagers();
 function getVillagers(){
     for (i=0; i < villagers.length; i++){
         var villagerUrl = 'https://www.instafluff.tv/ACDB/Villagers/' + villagers[i] + '.json'
