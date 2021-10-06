3236d850f28a18a2753ba3210f64f6767676b638
SpecsNStats <specsnstats@gmail.com>
2021-10-06 15:30:14 -0600

    fixed all filters

--- assets/script.js
+++ assets/script.js
@@ -61,7 +61,7 @@ function getFish() {
         .then(function (data) {
             data = Object.values(data);
             allFish.push(data)
-            console.log(allFish)
+            // console.log(allFish)
 
             $(data).each(function (i, e) {
                 // create new row under the header
@@ -98,7 +98,7 @@ function getFish() {
                 // add class to price cell
                 $(colPrice).addClass("price-item")
                 // add price to price cell
-                colPrice.innerText = e.price.toLocaleString();
+                colPrice.innerText = e.price;
                 // Create Months Cell
                 var colMonths = newRow.insertCell(6);
                 // add months to months cell
@@ -114,28 +114,6 @@ function getFish() {
                     checkbox.checked = fishValue == "true"
                 }
             });
-
-            var fishName = e.name["name-USen"];
-            console.log(fishName);
-            var checkbox = document.querySelector(
-                `input[data-fish-id="` + fishName + `"]`
-            );
-            console.log(checkbox, checkbox.checked);
-            var fishValue = localStorage.getItem(fishName);
-            console.log(fishValue, typeof fishValue);
-
-
-            if (localStorage.getItem(fishName) == undefined) {
-                localStorage.setItem(fishName, checkbox.checked);
-            } else {
-                checkbox.checked = fishValue == "true";
-            }
-
-            console.log(
-                colCheckbox
-                    .getElementsByTagName("input")[0]
-                    .getAttribute("data-fish-id")
-            );
         });
 };
 
@@ -198,19 +176,18 @@ function filterAll() {
             }
         })
     }
-
     // show only Sea
     if (locationSeaEl.checked == true) {
-        $("#location-sea").each(function () {
-            if (!($(this)[0].includes("Sea"))) {
+        $(".location-item").each(function () {
+            if (!($(this)[0].innerText.includes("Sea"))) {
                 $(this).parent().css('display', 'none')
             }
         })
     }
     // show only River
     if (locationRiverEl.checked == true) {
-        $("#location-river").each(function () {
-            if (!($(this)[0].includes("River"))) {
+        $(".location-item").each(function () {
+            if (!($(this)[0].innerText.includes("River"))) {
                 $(this).parent().css('display', 'none')
             }
         })
@@ -218,7 +195,7 @@ function filterAll() {
     // show only Pond
     if (locationPondEl.checked == true) {
         $(".location-item").each(function () {
-            if (!($(this)[0].includes("Pond"))) {
+            if (!($(this)[0].innerText.includes("Pond"))) {
                 $(this).parent().css('display', 'none')
             }
         })
@@ -226,30 +203,30 @@ function filterAll() {
     // show only Pier
     if (locationPierEl.checked == true) {
         $(".location-item").each(function () {
-            if (!($(this)[0].includes("Pier"))) {
+            if (!($(this)[0].innerText.includes("Pier"))) {
                 $(this).parent().css('display', 'none')
             }
         })
     }
     // show only 0-5000
     if (priceFilter0kEl.checked == true) {
-        $(".location-item").each(function () {
-            if ($(this)[0] > 5000) {
+        $(".price-item").each(function () {
+            if ($(this)[0].innerText > 5000) {
                 $(this).parent().css('display', 'none')
             }
         })
     }
     // show only 5001-10000
     if (priceFilter5kEl.checked == true) {
-        $(".location-item").each(function () {
-            if ($(this)[0] < 5000 || $(this)[0] > 9999) {
+        $(".price-item").each(function () {
+            if ($(this)[0].innerText < 5000 || $(this)[0].innerText > 9999) {
                 $(this).parent().css('display', 'none')
             }
         })
     }
     if (priceFilter10kEl.checked == true) {
-        $(".location-item").each(function () {
-            if ($(this)[0] < 10000) {
+        $(".price-item").each(function () {
+            if ($(this)[0].innerText < 10000) {
                 $(this).parent().css('display', 'none')
             }
         })
